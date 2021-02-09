b22a85d40e86d3c7bf8550b69a9f90c649dcb91d
thomasinho537 <tomas.schlogl@gym-tisnov.cz>
2021-02-09 21:52:09 +0100

    animace responzivni done + mobil done

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
-let moveUp = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".nadpis-box",
-    start: "bottom 40%",
-    end: "bottom 20%",
-    delay: 0.5,
-    scrub: 2,
-    markers: false,
-    toggleActions: "play none reverse none",
-  },
-});
+const mediaQuery = window.matchMedia("(min-width: 992px)");
 
-moveUp.fromTo(".cake_Img", { y: "10px" }, { y: "-60px" });
+// Check if the media query is true
+if (mediaQuery.matches) {
+  // Then trigger an alert
+  let moveUp = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".starter",
+      start: "bottom 40%",
+      end: "bottom 20%",
+      delay: 0.5,
+      scrub: 2,
+      markers: false,
+      toggleActions: "play none reverse none",
+    },
+  });
 
-let moveUp2 = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".nadpis-box",
-    start: "bottom 35%",
-    end: "bottom 25%",
-    delay: 0.5,
-    scrub: 2,
-    markers: false,
-    toggleActions: "play none reverse none",
-  },
-});
+  moveUp.fromTo(".cake_Img", { y: "10px" }, { y: "-90px" });
 
-moveUp2.fromTo(".about-box", { y: "10px" }, { y: "-85px" });
+  let moveUp2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".starter",
+      start: "bottom 35%",
+      end: "bottom 25%",
+      delay: 0.5,
+      scrub: 2,
+      markers: false,
+      toggleActions: "play none reverse none",
+    },
+  });
 
-//
+  moveUp2.fromTo(".about-box", { y: "10px" }, { y: "-45px" });
 
-let moveUp3 = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".nadpis-box",
-    start: "bottom 37.5%",
-    end: "bottom 25%",
-    delay: 0.5,
-    scrub: 2,
-    markers: true,
-    toggleActions: "play none reverse none",
-  },
-});
+  //
 
-moveUp3.fromTo(".doba-box", { y: "0px" }, { y: "-105px" });
+  let moveUp3 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".starter",
+      start: "bottom 37.5%",
+      end: "bottom 25%",
+      delay: 0.5,
+      scrub: 2,
+      markers: false,
+      toggleActions: "play none reverse none",
+    },
+  });
+
+  moveUp3.fromTo(".doba-box", { y: "0px" }, { y: "-30px" });
+}
